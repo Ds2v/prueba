ff211cf9736883544cefe0ea77cc7e05271d6e7b
Ds2v <dvelez.programar@gmail.com>
2023-12-15 16:13:48 -0500

    Haciendolo con IntelliJ IDEA

--- Molde.java
+++ Molde.java
@@ -1,7 +1,7 @@
 public class Molde {
+    public static void main(String[] args) {
 
-    public static void main String args[]{
-        
+        System.out.println("Vamos por aqui...");
+
     }
-
 }
